b63e16fde71e4d628d37d16d79429137870887d5
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2021-02-08 22:36:01 +0400

    Create BandPassFilter.m

---
+++ BandPassFilter.m
@@ -0,0 +1,25 @@
+function y = BandPassFilter(x, Fs, freq_range, W)
+    %Create the filter using 'fir1'
+    filt = fir1(W,freq_range/(Fs/2),'band')
+    %Apply the filter using 'conv'
+    y = conv(x,filt,'same')
+end
+
+
+Fs=10;
+x1 = repmat([1 0 -1 0],[1,10]); % this is a 2.5 Hz signal
+x2 = repmat([1 -1],[1,20]); % this is a 5 Hz signal
+x = x1 + x2;
+freq_range = [2,3]; % filter out the 5 Hz but keep the 2.5 Hz component
+W = 10;
+%%%%%%%%%%%%%%%%%
+y = BandPassFilter(x, Fs, freq_range, W);
+%%%%%%%%%%%%%%%%%
+% plot x1, x2, x=x1+x2, and BandPassFilter output
+t = 0:1/Fs:(length(y)-1)/Fs;
+plot(t(1:Fs),x1(1:Fs),'bo-')
+hold on
+plot(t(1:Fs),x2(1:Fs),'r*-')
+plot(t(1:Fs),x(1:Fs),'g')
+plot(t(1:Fs),y(1:Fs),'m--', 'LineWidth', 2)
+legend('x1-2.5Hz','x2-5.0Hz','x=x1+x2','output')
