97397d9a25323021db82cde5de90ba7b9da05005
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2021-02-09 20:46:59 +0400

    Update GaussianFilter.m

--- GaussianFilter.m
+++ GaussianFilter.m
@@ -1,6 +1,6 @@
 function y = GaussianFilter(x, sigma)
     % Compute W by rounding up 6*sigma
-    W = 6*sigma;
+    W = ceil(6*sigma);
     % Use fspecial to create our Gaussian filter
     gaussian_filter = fspecial('gaussian', [1,W], sigma);
     % convolve the signal with the filter
